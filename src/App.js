c7c013eeaa76cd58dcb8a154627bd41333d6804e
Coco <coco35532442@gmail.com>
2022-03-30 16:04:05 +0800

    設定router及文章API串接

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Nav from './components/Nav';
 import Products from './pages/Products';
 import ProductDetail from "./pages/ProductDetail";
 
+// 從 page 中匯入 Article, ArticleDetail 元件
+import Articles from './pages/Article';
+import ArticleDetail from "./pages/ArticleDetail";
+
 function App() {
   
   return (
@@ -16,8 +20,8 @@ function App() {
       <Routes>
         <Route path="products" element={<Products />} />
         <Route path="products/:productId" element={<ProductDetail />} />
-        <Route path="article" />
-        <Route path="article/:articleId" />
+        <Route path="article" element={<Articles />}  />
+        <Route path="article/:articleId" element={<ArticleDetail />} />
       </Routes>
     </BrowserRouter>
   )
